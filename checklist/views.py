ad2f61e9d4ca4ab4649a88cdf7481a191fe6563e
Juanpfrancos <juanpfrancos@programmer.net>
2023-11-01 23:44:41 -0500

    Add Date and employee filter.

--- checklist/views.py
+++ checklist/views.py
@@ -13,6 +13,24 @@ def crear_registro(request):
     return render(request, 'crear_registro.html', {'form': form})
 
 
+from django.utils.datetime_safe import datetime
+
 def lista_registros(request):
     registros = Registro.objects.all()
-    return render(request, 'lista_registros.html', {'registros': registros})
+    fechas = Registro.objects.values_list('fecha', flat=True).distinct()
+    empleados = Registro.objects.values_list('empleado', flat=True).distinct()
+
+    fecha_filtro = request.GET.get('fecha_filtro')
+    empleado_filtro = request.GET.get('empleado_filtro')
+
+    if fecha_filtro:
+        fecha_filtro = datetime.strptime(fecha_filtro, '%Y-%m-%d')  # Parsear la fecha
+        registros = registros.filter(fecha=fecha_filtro)
+    if empleado_filtro:
+        registros = registros.filter(empleado=empleado_filtro)
+
+    return render(request, 'lista_registros.html', {
+        'registros': registros,
+        'fechas': fechas,
+        'empleados': empleados,
+    })
